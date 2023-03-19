62b956922a3e42e81aaffdc78bd4dda064416fe6
pvpadrao <pv.padrao@gmail.com>
2023-03-19 16:19:13 -0400

    gifs added

--- PoseEstimationProject/pose_estimation_minimal_requirements.py
+++ PoseEstimationProject/pose_estimation_minimal_requirements.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import time
 
 # creating mediapipe pose objects
 mpPose = mp.solutions.pose
@@ -11,7 +12,7 @@ mpDraw = mp.solutions.drawing_utils
 # reading videos
 #cap = cv2.VideoCapture("PoseEstimationProject/videos/defante.mp4")
 cap = cv2.VideoCapture(0)
-
+previous_time = 0
 while True:
     sucess, img = cap.read()
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -25,8 +26,12 @@ while True:
             center_x, center_y = int(landmark.x * width), int(landmark.y * height)
             cv2.circle(img, (center_x, center_y), 8, (255, 0, 255), cv2.FILLED)
         mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
-    
-    #img = cv2.resize(img, (300, 620)) 
+    # calculting fps and printing to window
+    current_time = time.time()
+    fps = 1 / (current_time - previous_time)
+    previous_time = current_time
+    #img = cv2.resize(img, (300, 620))
+    cv2.putText(img, f'fps: {int(fps)}', (10, 70), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
 
