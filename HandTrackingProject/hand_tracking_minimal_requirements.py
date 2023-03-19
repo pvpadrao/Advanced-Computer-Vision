d9c71856269a526363e534e86c058f8c4c80f500
pvpadrao <pv.padrao@gmail.com>
2023-03-19 15:38:46 -0400

    .

--- HandTrackingProject/hand_tracking_minimal_requirements.py
+++ HandTrackingProject/hand_tracking_minimal_requirements.py
@@ -38,7 +38,6 @@ while True:
     current_time = time.time()
     fps = 1 / (current_time - previous_time)
     previous_time = current_time
-    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 0), 3)
-
+    cv2.putText(img, f'fps: {int(fps)}', (10, 70), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
