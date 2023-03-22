faeba06e6adf15cdaeae3b81780fbb625b2a61f2
pvpadrao <pv.padrao@gmail.com>
2023-03-22 13:30:14 -0400

    template updated

--- HandTrackingProject/template_opening_webcam.py
+++ HandTrackingProject/template_opening_webcam.py
@@ -1,10 +1,14 @@
 import cv2
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 
-while True:
+while cap.isOpened():
     sucess, img = cap.read()
 
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
